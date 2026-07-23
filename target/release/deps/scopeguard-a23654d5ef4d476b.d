311e76c55ffdd7a4eda2ca0a077a6956d378dc50
Prince Kumar <shinghsakshi59@gmail.com>
2026-07-24 00:51:55 +0530

    Add files via upload
    f

---
+++ target/release/deps/scopeguard-a23654d5ef4d476b.d
@@ -0,0 +1,7 @@
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/scopeguard-a23654d5ef4d476b.d: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
+
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/libscopeguard-a23654d5ef4d476b.rlib: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
+
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/libscopeguard-a23654d5ef4d476b.rmeta: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
+
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs:
---
+++ target/release/deps/serde-938ba71036b0f0db.d
@@ -0,0 +1,14 @@
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/serde-938ba71036b0f0db.d: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/lib.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/integer128.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/mod.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/de.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/ser.rs /home/basant/Code/OpratingSystem/BharatOS/target/release/build/serde-666fc025e08bfa03/out/private.rs
+
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/libserde-938ba71036b0f0db.rlib: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/lib.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/integer128.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/mod.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/de.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/ser.rs /home/basant/Code/OpratingSystem/BharatOS/target/release/build/serde-666fc025e08bfa03/out/private.rs
+
+/home/basant/Code/OpratingSystem/BharatOS/target/release/deps/libserde-938ba71036b0f0db.rmeta: /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/lib.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/integer128.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/mod.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/de.rs /home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/ser.rs /home/basant/Code/OpratingSystem/BharatOS/target/release/build/serde-666fc025e08bfa03/out/private.rs
+
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/lib.rs:
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/integer128.rs:
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/mod.rs:
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/de.rs:
+/home/basant/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.229/src/private/ser.rs:
+/home/basant/Code/OpratingSystem/BharatOS/target/release/build/serde-666fc025e08bfa03/out/private.rs:
+
+# env-dep:OUT_DIR=/home/basant/Code/OpratingSystem/BharatOS/target/release/build/serde-666fc025e08bfa03/out
